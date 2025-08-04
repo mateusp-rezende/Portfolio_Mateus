3c6ab37f8a4f5563cf73be04d573e36e9b93ded1
Mateus de Paula Rezende <110778640+mateusp-rezende@users.noreply.github.com>
2025-08-04 12:35:41 -0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -19,11 +19,12 @@ function Home() {
 
         <FlipCard
     href="https://alfred-gerenciador.vercel.app/"
+      <p>Projeto completo: https://github.com/mateusp-rezende/gestor-pedidos-microservicos</p>
     src={gestordepedidos} 
     nome="ALFRED GERENCIADOR"
    
    
-    Descricao="API para gestão de produtos, clientes e pedidos em arquitetura de microsserviços •  Eureka • API Gateway •  OpenFeign • Tech Stack: Spring Cloud, Spring Boot , JPA, MySQL Projeto completo: https://github.com/mateusp-rezende/gestor-pedidos-microservicos"
+    Descricao="API para gestão de produtos, clientes e pedidos em arquitetura de microsserviços •  Eureka • API Gateway •  OpenFeign • Tech Stack: Spring Cloud, Spring Boot , JPA, MySQL "
 />
   
          <FlipCard
